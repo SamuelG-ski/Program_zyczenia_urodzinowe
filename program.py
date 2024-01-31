7d737ab68776eee07c1f756413e767f91e4acbf1
Samuel <jay.jotzet@wp.pl>
2024-01-31 09:19:58 +0100

    Ukończone zadanie domowe

---
+++ program.py
@@ -0,0 +1,30 @@
+print("Witaj w prgramie spersonalizowanych życzeń urodzinowych!")
+
+print()
+print("Podaj imię odbiorcy:")
+recipient_name = input()
+
+print()
+print("Podaj rok urodzenia odbiorcy:")
+year_born = int(input())
+
+print()
+print("Podaj spersonalizowaną wiadomośc:")
+personalized_message = input()
+
+print()
+print("Podaj imię nadawcy:")
+sender_name = input()
+
+actual_year = int(2024)
+
+age = actual_year - year_born
+
+print()
+print(f"{recipient_name}, wszystkiego najlepszego z okazji {age} urodzin!")
+
+print()
+print(f"{personalized_message}")
+
+print()
+print(f"{sender_name}")
